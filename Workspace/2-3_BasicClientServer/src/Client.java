59b5da8ce6a963b57a13740766888ff489a51a27
jasonthorne <jasonthorne@live.ie>
2021-04-24 01:02:07 +0100

    basic client server 3

---
+++ Workspace/2-3_BasicClientServer/src/Client.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		
+		try {
+			//client socket with IP address and port number of machine where server is running (same IP address in this local example):
+			/** get this from db when attempting to connect. Have server send this info when it fires up */
+			Socket socket = new Socket(/*"127.0.0.1"*/ "192.168.1.100", 999);
+			
+			InputStream inputStream = socket.getInputStream(); //for retrieving info FROM server
+			OutputStream outputStream = socket.getOutputStream(); //for sending TO server
+			
+			//send a message to the server (as bytes):
+			outputStream.write("Hullo from client!".getBytes());
+			
+			//response from server:
+			byte[] buffer = new byte[1024]; //create array buffer for stream
+			inputStream.read(buffer); //read buffer numb of bytes (1024) of input stream and store in buffer
+			
+			//print message from server:
+			System.out.println("message from seerver: "+ new String(buffer).trim());
+			
+			/** finally, CLOSE SOCKET: */
+			socket.close();
+			
+		} catch (IOException e) {
+			
+			System.out.println("uh oh!");
+			e.printStackTrace();
+		}
+	}
+
+}
--- Workspace/2-3_BasicClientServer/src/Server.java
+++ Workspace/2-3_BasicClientServer/src/Server.java
@@ -1,19 +1,67 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Server {
+	
+	/** RUN SERVER FIRST - as this opens a socket on port 999 & waits for client to connect to it */
 
 	public static void main(String[] args) {
 		
+		
+		/////https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
+		//===============================
+		InetAddress IP;
+		try {
+			IP = InetAddress.getLocalHost();
+			System.out.println("IP of my system is: " + IP.getHostAddress());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//====================================
+		
+		//-----------------------------
+		
+		try(final DatagramSocket socket = new DatagramSocket()){
+			  socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+			  String ip = socket.getLocalAddress().getHostAddress();
+			  System.out.println("IP of my system is: " + ip);
+			  
+			} catch (SocketException e1) {
+				e1.printStackTrace();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		
+		//this is prob best - as described in stackoverflow!! 
+		
+		//----------------------------
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		//server socket with a given port number (if port numb is available then obj is created)
 		try {
-			ServerSocket serverSocket = new ServerSocket(999);
+			ServerSocket serverSocket = new ServerSocket(999); 
 			
-			//this waits for a connection to be made to this (from client) then accepts it:
+			System.out.println("server waiting for client");
+			
+			//this waits for a connection to be made to serverSocket (from client) then creates a new socket with that connection
 			Socket socket = serverSocket.accept();
+			
+			System.out.println("client connected. IP of server is: " + socket.getInetAddress());
 			
 			//socket has an input & output stream:
 			InputStream inputStream = socket.getInputStream(); /** what's read here is what's sent BY the client */
@@ -22,7 +70,7 @@ public class Server {
 			//create array buffer for stream:
 			byte[] buffer = new byte[1024];
 			
-			 //read buffer numb of bytes (1024) into input stream:
+			 //read buffer numb of bytes (1024) into input stream and store in buffer:
 			inputStream.read(buffer);
 			
 			System.out.println("recieved from client: " + new String(buffer).trim()); //trim unnecessary characters
@@ -30,11 +78,15 @@ public class Server {
 			//write message to output stream (as bytes)
 			outputStream.write("hello from server!".getBytes());
 			
+			/** finally, CLOSE SOCKETS: */
+			
+			socket.close();
+			serverSocket.close();
 			
 		} catch (IOException e) {
 			System.out.println("port number not available");
 			e.printStackTrace();
-		}
+		} 
 		
 		
 
