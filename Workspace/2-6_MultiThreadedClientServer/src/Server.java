88ebce67f243bbc8a89696e7cb7268f102767a20
jasonthorne <jasonthorne@live.ie>
2021-04-25 18:51:30 +0100

    multi threaded client server 1

--- Workspace/2-6_MultiThreadedClientServer/src/Server.java
+++ Workspace/2-6_MultiThreadedClientServer/src/Server.java
@@ -6,10 +6,7 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
-/** 
- * this is a single threaded application. 
- * Which is BAD as if a client connects to this server, and is blocked, the server then also becomes blocked
- * */
+/** MULTI threaded application. */
 
 class QuoteService {
 	
@@ -49,29 +46,7 @@ public class Server {
 				System.out.println("waiting for client");
 				Socket socket = serverSocket.accept(); 
 				
-				//input & output streams for socket:
-				InputStream inputStream = socket.getInputStream();
-				OutputStream outputStream = socket.getOutputStream();
 				
-				System.out.println("waiting for product info from client");
-				
-				byte request[] = new byte[100];
-				inputStream.read(request); //read from inputStream and store in request (100 bytes as that the size of request)
-				
-				//create product from bytes held in request:
-				String product = new String(request).trim();
-				
-				System.out.println("recieved product: " + product);
-				
-				//get price of product from quoteSerice:
-				String price = quoteService.getQuote(product);
-				
-				if(price == null) { System.out.println("invalid product"); }
-					
-				//sent the price back to the client:
-				outputStream.write(price.getBytes());
-				
-				System.out.println("response sent.");
 				
 				/** close socket: */
 				socket.close();
