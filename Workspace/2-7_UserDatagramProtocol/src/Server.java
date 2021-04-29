ef2878604a26b2c8daded85f936fe236919a2a64
jasonthorne <jasonthorne@live.ie>
2021-04-29 16:40:25 +0100

    UserDatagramProtocol 3

--- Workspace/2-7_UserDatagramProtocol/src/Server.java
+++ Workspace/2-7_UserDatagramProtocol/src/Server.java
@@ -1,4 +1,14 @@
 
+
+/** UDP is CONNECTIONLESS. 
+ * you dont have to first establish the connection like you do with TCP (previous examples are all TCP)
+ * You still need a socket though (and end point to send the data to. But it is not connection oriented)
+ * Data is sent with a DATAGRAM PACKET.
+ * think of TCP as like using a telephone, where a connection has to be established before data can be sent.
+ * Think of UDP as like sending an envelope through the mail 
+ * 
+ * */
+
 public class Server {
 
 	public static void main(String[] args) {
