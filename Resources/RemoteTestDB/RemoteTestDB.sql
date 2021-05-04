5e1bd2796614ffe6e93bc9fb9b110f76e70535a7
jasonthorne <jasonthorne@live.ie>
2021-05-04 22:00:40 +0100

    remote test db

---
+++ Resources/RemoteTestDB/RemoteTestDB.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS remote_test_db;
+CREATE DATABASE remote_test_db;
+
+USE remote_test_db;
+
+CREATE TABLE users (
+	userID INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64) DEFAULT NULL,
+	ip INT DEFAULT 0,
+	port INT DEFAULT 0,
+	PRIMARY KEY (userID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+INSERT INTO users (name, ip, port) VALUES ("user1", 1, 00);
+INSERT INTO users (name, ip, port) VALUES ("user2", 2, 01);
+INSERT INTO users (name, ip, port) VALUES ("user3", 3, 02);
